cd153e697d4ba96b6e81906aff2de6d51ffb010c
aachalv <aachal.vartak@raweng.com>
2021-01-19 01:09:09 +0530

    Contentstack styling completed

---
+++ pages/contentstack.js
@@ -0,0 +1,22 @@
+import styles from "../styles/Contentstack.module.css";
+import Navigation from "../components/navigation/Navigation";
+import Header from "../components/header/Header";
+import Section from "../components/section/Section";
+import Footer from "../components/footer/Footer";
+
+export default function Surfboard({ data }) {
+  return (
+    <div>
+      <Navigation styles={styles} navigationLinks={data[0].navigationLinks} />
+      <Header styles={styles} header={data[0].header} />
+      <Section styles={styles} section={data[0].section} />
+      <Footer styles={styles} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:5000/companyList/company/cs");
+  const companyData = await response.json();
+  return { props: { ...companyData } };
+}
